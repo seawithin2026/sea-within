db1342fdf01533209324f1ab94a9f2fed951b5b5
Marilyn <marilyn.breau22@gmail.com>
2026-05-05 21:02:32 -0300

    fixe the y"-20

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -24,8 +24,8 @@ export default function Navigation() {
 
   return (
     <motion.nav
-      initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       transition={{ duration: 1, delay: 0.5 }}
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-700 ${
         isScrolled
