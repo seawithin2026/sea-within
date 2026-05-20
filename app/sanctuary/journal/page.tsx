2ea6fae76fb1057a1bfeeacc831c856f0cff8bb6
Marilyn <marilyn.breau22@gmail.com>
2026-05-19 23:32:06 -0300

    change calc

--- app/sanctuary/journal/page.tsx
+++ app/sanctuary/journal/page.tsx
@@ -606,7 +606,7 @@ export default function JournalPage() {
     .journal-parchment {
 width: auto !important;
   height: calc(100vh - 120px) !important; /* fills screen but leaves room */
-  max-height: calc(100vh - 120px) !important;
+  max-height: calc(100vh - 90px) !important;
   object-fit: contain !important;
 
   /* Center horizontally */
