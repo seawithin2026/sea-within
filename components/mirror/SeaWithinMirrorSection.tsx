f6361c778d68d6cd4bd79c8105cda3047828f8de
Marilyn <marilyn.breau22@gmail.com>
2026-05-27 17:24:31 -0300

    item cenrer return video area

--- components/mirror/SeaWithinMirrorSection.tsx
+++ components/mirror/SeaWithinMirrorSection.tsx
@@ -30,7 +30,7 @@ export function SeaWithinMirrorSection({
         grid-cols-1 
         lg:grid-cols-[minmax(0,1.7fr)_minmax(0,1fr)]   /* ← BIGGER LEFT SIDE */
         gap-12 
-        items-start
+        items-center
         w-full
       ">
 
