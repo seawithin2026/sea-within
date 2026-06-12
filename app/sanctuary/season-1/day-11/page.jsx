8b10f91e92b5f55a33240c51eb4c9687f469ee5e
Marilyn <marilyn.breau22@gmail.com>
2026-06-12 14:39:28 -0300

    change vid speed

--- app/sanctuary/season-1/day-11/page.jsx
+++ app/sanctuary/season-1/day-11/page.jsx
@@ -16,7 +16,7 @@ export default function Day11Page() {
           muted
           playsInline
           className="day11-hero-video"
-          onLoadedMetadata={(e) => (e.currentTarget.playbackRate = 0.2)}
+          onLoadedMetadata={(e) => (e.currentTarget.playbackRate = 0.4)}
         />
 
         <div className="day11-hero-text">
