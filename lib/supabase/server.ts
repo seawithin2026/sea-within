e1d45f22c08502bfd8b9b85f4aefa39153bfb74a
Marilyn <marilyn.breau22@gmail.com>
2026-09-03 16:13:36 -0300

    change for the cookie being added to the server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,12 +1,20 @@
-import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
 
 export function supabaseServer() {
+  const cookieStore = cookies();
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies
+      cookies: {
+        get(name) {
+          return cookieStore.get(name)?.value;
+        },
+        set() {},
+        remove() {},
+      },
     }
   );
 }
