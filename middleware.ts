318f070cccb4f29d67b54015dc34018609429270
Marilyn <marilyn.breau22@gmail.com>
2026-09-03 14:06:29 -0300

    added middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import { createServerClient } from "@supabase/ssr";
+
+export async function middleware(req) {
+  const res = NextResponse.next();
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    { cookies: req.cookies }
+  );
+
+  await supabase.auth.getSession();
+
+  return res;
+}
