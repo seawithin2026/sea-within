8d81b90598bddc1b1085f2cadd630ef679d98feb
Marilyn <marilyn.breau22@gmail.com>
2026-09-03 17:33:29 -0300

    fix webhook

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: NextRequest) {
 
   // Supabase client
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
 
