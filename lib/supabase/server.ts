d44ccf22d4a64909bffdbca5563f70575953f75a
Marilyn <marilyn.breau22@gmail.com>
2026-09-23 15:06:23 -0300

    fix

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -13,10 +13,10 @@ export function supabaseServer() {
           return cookieStore.get(name)?.value;
         },
         set(name, value, options) {
-          cookieStore.set({ name, value, ...options });
+          cookieStore.set(name, value, options);
         },
         remove(name, options) {
-          cookieStore.set({ name, value: "", ...options });
+          cookieStore.set(name, "", options);
         },
       },
     }
